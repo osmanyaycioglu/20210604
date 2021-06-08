ca0995e65260af69fd2593daab3324e5f9f2b693
osman <osman@osman123.com>
2021-06-08 16:56:52 +0300

    SON

--- ms-order/src/main/java/com/training/spring/order/rest/OrderRest.java
+++ ms-order/src/main/java/com/training/spring/order/rest/OrderRest.java
@@ -1,6 +1,7 @@
 package com.training.spring.order.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,9 +18,12 @@ public class OrderRest {
     @Autowired
     private OrderService os;
 
+    @Value("${server.port}")
+    private int          port;
+
     @PostMapping("/place")
     public String place(@RequestBody final Order orderParam) throws MyRestException {
-        return this.os.placeOrder(orderParam);
+        return this.os.placeOrder(orderParam) + " order : " + this.port;
     }
 
 }
